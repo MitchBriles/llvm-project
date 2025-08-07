47bb9731762fd35b18620bba1e36fab8dcec04bc
Aaditya <Aaditya.AlokDeshpande@amd.com>
2025-08-07 11:38:46 +0530

    Removing `break` before `else`

--- llvm/lib/Target/AMDGPU/SIISelLowering.cpp
+++ llvm/lib/Target/AMDGPU/SIISelLowering.cpp
@@ -5369,7 +5369,6 @@ static MachineBasicBlock *lowerWaveReduce(MachineInstr &MI,
               BuildMI(BB, MI, DL, TII->get(AMDGPU::S_MUL_I32), DstReg)
                   .addReg(SrcReg)
                   .addReg(ParityRegister);
-              break;
             } else {
               Register DestSub0 =
                   MRI.createVirtualRegister(&AMDGPU::SReg_32RegClass);
@@ -5398,8 +5397,8 @@ static MachineBasicBlock *lowerWaveReduce(MachineInstr &MI,
                   .addImm(AMDGPU::sub0)
                   .addReg(DestSub1)
                   .addImm(AMDGPU::sub1);
-              break;
             }
+            break;
           }
       case AMDGPU::S_SUB_I32: {
         Register NegatedVal = MRI.createVirtualRegister(DstRegClass);
